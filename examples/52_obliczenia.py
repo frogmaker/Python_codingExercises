71325e05615c442ba2f8e3f20087320ae08dc847
frogmaker <frogmaker.pi@gmail.com>
2022-12-21 14:18:37 +0100

    20221122d_moduły

---
+++ examples/52_obliczenia.py
@@ -0,0 +1,13 @@
+import figury
+
+# wybor = input("""Wybierz figurę, której pole chcesz obliczyć:
+# 1. Kwadrat
+# 2. Prostokąt
+# 3. Koło
+# 4. Trójkąt
+# 5. Trapez
+# """)
+#
+# if (wybor == '1'):
+#     a = float(input("Podaj bok kwadratu: "))
+#     print("Pole kwadratu wynosi:", figury.pole_kwadratu(a))
