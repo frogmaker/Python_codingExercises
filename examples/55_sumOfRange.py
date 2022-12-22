e026ea06616be3eb148c2c7834f322345458295e
frogmaker <frogmaker.pi@gmail.com>
2022-12-22 20:44:32 +0100

    20221122z_pomiarCzasuDziałaniaKodu

--- examples/55_sumOfRange.py
+++ examples/55_sumOfRange.py
@@ -1,4 +1,7 @@
 
+
+import time
+
 rangeToSum: int = 5 #int(input("Podaj liczbę będącą końcem zakresu: "))
 
 print('sposób 1')
@@ -50,5 +53,33 @@ print('sposób 5')
 def sum5(a):
     return sum((number for number in range(a+1)))
 
-print(sum4(rangeToSum))
+print(sum5(rangeToSum))
 print((number for number in range(rangeToSum+1)))
+
+print()
+print("ocena czasu działania kodu: ")
+
+def finish_timer(start):
+    end = time.perf_counter()
+    return end - start
+
+
+start = time.perf_counter()
+print(sum1(234567))
+print(finish_timer(start))
+
+start = time.perf_counter()
+print(sum2(234567))
+print(finish_timer(start))
+
+start = time.perf_counter()
+print(sum3(234567))
+print(finish_timer(start))
+
+start = time.perf_counter()
+print(sum4(234567))
+print(finish_timer(start))
+
+start = time.perf_counter()
+print(sum5(234567))
+print(finish_timer(start))
