ce901296d4e45e2bc9c6ff92d6387419cf5b51cd
frogmaker <frogmaker.pi@gmail.com>
2022-12-25 11:27:01 +0100

    20221125_1_funkcja jako argument innej funkcji - poprawki i dodatkowy przykład użycia

--- examples/55_sumOfRange.py
+++ examples/55_sumOfRange.py
@@ -1,8 +1,7 @@
 
-
 import time
 
-rangeToSum: int = 5 #int(input("Podaj liczbę będącą końcem zakresu: "))
+rangeToSum: int = 5  # int(input("Podaj liczbę będącą końcem zakresu: "))
 
 print('sposób 1')
 def sum1(a):
@@ -15,46 +14,45 @@ def sum1(a):
 
 print("suma liczb z podanego zakresu to: ", sum1(rangeToSum))
 
-#sposób 2
+# sposób 2
 print('sposób 2')
 
 
 def sum2(a):
-    return sum([number for number in range(a+1)])
+    return sum([number for number in range( a +1)])
 
 print(sum2(rangeToSum))
 #
 
-print(sum([number2 for number2 in range(rangeToSum+1)]))
-print([number for number in range(rangeToSum+1)])
-#sposób 3
+print(sum([number2 for number2 in range(rangeToSum +1)]))
+print([number for number in range(rangeToSum +1)])
+# sposób 3
 print('sposób 3')
 
 def sum3(a):
-    return (((a))*(a+1))/2
+    return (((a) ) *( a +1) ) /2
 
 print(int(sum3(rangeToSum)))
 
-#sposób 4
+# sposób 4
 print('sposób 4')
 
 
 def sum4(a):
-    return sum({number for number in range(a+1)})
+    return sum({number for number in range( a +1)})
 
 print(sum4(rangeToSum))
-print({number for number in range(rangeToSum+1)})
+print({number for number in range(rangeToSum +1)})
 
-
-#sposób 5
+# sposób 5
 print('sposób 5')
 
 
 def sum5(a):
-    return sum((number for number in range(a+1)))
+    return sum((number for number in range( a +1)))
 
 print(sum5(rangeToSum))
-print((number for number in range(rangeToSum+1)))
+print((number for number in range(rangeToSum +1)))
 
 print()
 print("ocena czasu działania kodu: ")
@@ -85,7 +83,7 @@ print(sum5(234567))
 print(finish_timer(start))
 
 
-#funkcja jako zmienna _57
+# funkcja jako zmienna _57
 def function(func):
     func()
 
@@ -97,6 +95,17 @@ def show_message():
 function(show_message)
 
 
+def function2(func, arg):
+    func(arg)
+
+
+def show_message2(message):
+    print(message)
+
+
+function2(show_message2, "inna wiadomość")
+
+
 def function_performance(func, arg):
     start = time.perf_counter()
     func(arg)
@@ -104,8 +113,8 @@ def function_performance(func, arg):
     return end - start
 
 
-print("a ", function_performance(sum1, 50000000))
-print("b ", function_performance(sum2, 50000000))
-print("c ", function_performance(sum3, 50000000))
-print("d ", function_performance(sum4, 50000000))
-print("e ", function_performance(sum5, 50000000))
+print("a ", function_performance(sum1, 5000000))
+print("b ", function_performance(sum2, 5000000))
+print("c ", function_performance(sum3, 5000000))
+print("d ", function_performance(sum4, 5000000))
+print("e ", function_performance(sum5, 5000000))
