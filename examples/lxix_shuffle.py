9639a1e4a6b310dfc23abac7cf82dea19436ecaf
frogmaker <frogmaker.pi@gmail.com>
2022-12-31 10:52:10 +0100

    20221230_1_LXVII+LXIX: shaffle

---
+++ examples/lxix_shuffle.py
@@ -0,0 +1,22 @@
+"""
+shuffle - randomizes entire list
+
+"""
+
+import random
+
+cardList = ["9", "9", "9", "9",
+            "10", "10", "10", "10",
+            "Jack", "Jack", "Jack", "Jack",
+            "Queen", "Queen", "Queen", "Queen",
+            "King", "King", "King", "King",
+            "Ace", "Ace", "Ace", "Ace",
+            "Joker", "Joker"]
+
+
+random.shuffle(cardList)
+
+print(cardList)
+
+
+
